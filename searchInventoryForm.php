32145cfd9f852162e2452e9581213d024ffda357
Michael <mjlacy.dude@gmail.com>
2017-09-15 12:55:46 -0500

    All appears to be normalized.

--- searchInventoryForm.php
+++ searchInventoryForm.php
@@ -13,13 +13,39 @@ if(isset($_SESSION['id'])) {
 
     echo "<br>&nbsp&nbspEnter what criteria you would like to see any matching items for.
         <form action ='searchInventoryResults.php' method ='POST'><br>";
-            for($count = 0; $count< count($columnNames); $count++){
+            for($count = 0; $count < 4; $count++){
+                $columnName = $columnNames[$count];
+                if($count == 2){
+                    echo "&nbsp&nbsp<label>Type</label> <br>&nbsp&nbsp<select name='Type'><option value=''></option>";
+                    $sql2 = "SELECT Type FROM subtypes;";
+                    $result2 = mysqli_query($conn, $sql2);
+                    while($TypeRow = mysqli_fetch_array($result2)){
+                        echo "<option value= ". $TypeRow['Type'].">".$TypeRow['Type']."</option>";
+                    }
+                    echo "</select><br><br>";
+                }
+                elseif($count == 3){
+                    $sql3 = "SELECT Subtype FROM subtypes";
+                    $result3 = mysqli_query($conn, $sql3);
+                    echo "&nbsp&nbsp<label>Subtype</label> <br>&nbsp&nbsp<select name='Subtype'><option value=''></option>";
+                    while($SubtypeRow = mysqli_fetch_array($result3)){
+                        echo "<option value= ". $SubtypeRow['Subtype'].">".$SubtypeRow['Subtype']."</option>";
+                    }
+                    echo "</select>";
+                }
+                else{
+                    echo "&nbsp&nbsp<label>$columnNames[$count]</label> <br>&nbsp&nbsp<input type='text' name=".$columnName
+                        . " value=" . $row[$columnNames[$count]] . "><br><br>";
+                }
+            }
+            echo "<br><br>";
+            for($count = 3; $count< count($columnNames); $count++){
                 $isSelect = false;
                 $columnName = $columnNames[$count];
-                $sql2 = "SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS 
-                WHERE table_name = 'inventory' AND COLUMN_NAME = '$columnNames[$count]';";
-                $result2 = mysqli_query($conn, $sql2);
-                $rowType = mysqli_fetch_array($result2);
+                $sql4 = "SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS 
+                        WHERE table_name = 'inventory' AND COLUMN_NAME = '$columnNames[$count]';";
+                $result4 = mysqli_query($conn, $sql4);
+                $rowType = mysqli_fetch_array($result4);
                 if($rowType['DATA_TYPE'] == "tinyint"){
                     $isSelect = true;
                     $inputs = "&nbsp&nbsp<label>$columnNames[$count]</label> <br>&nbsp&nbsp<select name=";
@@ -32,7 +58,7 @@ if(isset($_SESSION['id'])) {
                 }
                 if($isSelect){
                     $inputs .= $columnName . "><option value=''></option>
-                    <option value=1>Yes</option><option value=0>No</option></select><br><br>";
+                            <option value=1>Yes</option><option value=0>No</option></select><br><br>";
                 }
                 else{
                     $inputs .= $columnName . " value=" . $row[$columnNames[$count]] . "><br><br>";
--- searchInventoryResults.php
+++ searchInventoryResults.php
@@ -11,35 +11,41 @@ include 'dbh.php';
 
 if(isset($_SESSION['id'])) {
 
-$columnNames = array();
-$receivedValues = array();
+    $columnNames = array();
+    $receivedValues = array();
 
-$sql="SHOW COLUMNS FROM inventory";
-$result = mysqli_query($conn, $sql);
-while($row = mysqli_fetch_array($result)) {
-    array_push($columnNames, $row['Field']);
-    if(strpos($row['Field'],' ')){
-        $columnName = str_replace(" ","", $row['Field']);
-        array_push($receivedValues, $_POST[$columnName]);
+    $sql="SHOW COLUMNS FROM inventory";
+    $result = mysqli_query($conn, $sql);
+    while($row = mysqli_fetch_array($result)) {
+        array_push($columnNames, $row['Field']);
+        if(strpos($row['Field'],' ')){
+            $columnName = str_replace(" ","", $row['Field']);
+            array_push($receivedValues, $_POST[$columnName]);
+        }
+        else{
+            array_push($receivedValues, $_POST[$row['Field']]);
+        }
     }
-    else{
-        array_push($receivedValues, $_POST[$row['Field']]);
+
+    $tableHeadNeeded = true;
+    $outerCount = 0;
+    $sql = "SELECT * FROM inventory WHERE ";
+    $andNeeded = false;
+
+    for($count = 0; $count< count($columnNames); $count++){
+        if($receivedValues[$count] !== ""){
+            $sql .= "`" . $columnNames[$count] . "`" . " = '" . $receivedValues[$count]. "' AND ";
+            error_reporting(E_ERROR | E_PARSE);
+        }
     }
-}
 
-$tableHeadNeeded = true;
-$outerCount = 0;
-$sql = "SELECT * FROM inventory WHERE ";
-$andNeeded = false;
+    $sql = chop($sql," AND ");
 
-for($count = 0; $count< count($columnNames); $count++){
-    if($receivedValues[$count] !== ""){
-        $sql .= "`" . $columnNames[$count] . "`" . " = '" . $receivedValues[$count]. "' AND ";
-        error_reporting(E_ERROR | E_PARSE);
+    if($_POST['Type'] !== ""){
+        $sql .= " AND Subtype IN (SELECT Subtype FROM subtypes WHERE type = '". $_POST['Type']."')";
     }
-}
 
-$sql = chop($sql," AND ") .";";
+    $sql .= ";";
 
     if($sql == "SELECT * FROM inventory WHERE;"){
         echo "<br> Please fill out at least 1 search field.";
@@ -48,38 +54,50 @@ $sql = chop($sql," AND ") .";";
               </form>";
         exit();
     }
-
     $result = mysqli_query($conn, $sql);
     while($row = mysqli_fetch_array($result)) {
         if($tableHeadNeeded){
             $tableHeadNeeded = false;
             $outerCount++;
             echo "<table>";
-            for($count = 0; $count< count($columnNames); $count++){
+            for($count = 0; $count< 2; $count++){
+                echo "<th>$columnNames[$count]</th>";
+            }
+            echo "<th>Type</th>";
+            for($count = 2; $count< count($columnNames); $count++){
                 echo "<th>$columnNames[$count]</th>";
             }
         }
         echo "<tr>";
         for($count = 0; $count< count($columnNames); $count++){
-            $sql2 = "SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS 
-                WHERE table_name = 'inventory' AND COLUMN_NAME = '$columnNames[$count]';";
-            $result2 = mysqli_query($conn, $sql2);
-            $rowType = mysqli_fetch_array($result2);
-            if($rowType['DATA_TYPE'] == "tinyint"){
-                if($row[$columnNames[$count]] == 0){
-                    echo '<td>No</td>';
-                }
-                else{
-                    echo '<td>Yes</td>';
-                }
+            if($count == 1){
+                echo '<td> '.$row[$columnNames[$count]].'</td>';
+                $innerSQL = "SELECT Type FROM subtypes WHERE Subtype = '".$row[$columnNames[$count + 1]]."';";
+                $innerResult = mysqli_query($conn, $innerSQL);
+                $innerRow = mysqli_fetch_array($innerResult);
+                echo '<td>'. $innerRow['Type'].'</td>';
             }
             else{
-                echo '<td> '.$row[$columnNames[$count]].'</td>';
+                $sql2 = "SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS 
+                WHERE table_name = 'inventory' AND COLUMN_NAME = '$columnNames[$count]';";
+                $result2 = mysqli_query($conn, $sql2);
+                $rowType = mysqli_fetch_array($result2);
+                if($rowType['DATA_TYPE'] == "tinyint"){
+                    if($row[$columnNames[$count]] == 0){
+                        echo '<td>No</td>';
+                    }
+                    else{
+                        echo '<td>Yes</td>';
+                    }
+                }
+                else{
+                    echo '<td> '.$row[$columnNames[$count]].'</td>';
+                }
             }
         }
         echo "<td> <a href='editInventory.php?edit=$row[inv_id]'>Edit<br></td>
                 <td> <a href='includes/deleteInventory.inc.php?delete=$row[inv_id]'>Delete<br></td>
-            </tr><br>";
+            </tr>";
     }
 
     if($outerCount == 0) {

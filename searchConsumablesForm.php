ae4fc7d4eaee0439472af65e8531960068fa0a98
Michael <mjlacy.dude@gmail.com>
2017-10-20 13:53:50 -0500

    Removed id column.

--- searchConsumablesForm.php
+++ searchConsumablesForm.php
@@ -14,35 +14,34 @@ if(isset($_SESSION['id'])) {
 
     echo "<br>&nbsp&nbspEnter what criteria you would like to see any matching items for.
         <form action ='searchConsumablesResults.php' method ='POST'><br>";
-    for($count = 0; $count < 4; $count++){
+    for($count = 0; $count < 3; $count++){
         $columnName = $columnNames[$count];
-        if($count == 2){
+        if($count == 1){
             echo "&nbsp&nbsp<label>Type</label> <br>&nbsp&nbsp<select name='Type'><option value=''></option>";
             $sql2 = "SELECT Type FROM subtypes;";
             $result2 = mysqli_query($conn, $sql2);
             while($TypeRow = mysqli_fetch_array($result2)){
                 if(!in_array($TypeRow['Type'], $Types)){
                     array_push($Types, $TypeRow['Type']);
-                    echo "<option value= ". $TypeRow['Type'].">".$TypeRow['Type']."</option>";
+                    echo "<option value= '". $TypeRow['Type']."'>".$TypeRow['Type']."</option>";
                 }
             }
             echo "</select><br><br>";
-        }
-        elseif($count == 3){
+//        }
+//        elseif($count == 2){
             $sql3 = "SELECT Subtype FROM subtypes";
             $result3 = mysqli_query($conn, $sql3);
             echo "&nbsp&nbsp<label>Subtype</label> <br>&nbsp&nbsp<select name='Subtype'><option value=''></option>";
             while($SubtypeRow = mysqli_fetch_array($result3)){
-                echo "<option value= ". $SubtypeRow['Subtype'].">".$SubtypeRow['Subtype']."</option>";
+                echo "<option value= '". $SubtypeRow['Subtype']."'>".$SubtypeRow['Subtype']."</option>";
             }
-            echo "</select>";
+            echo "</select><br><br>";
         }
         else{
             echo "&nbsp&nbsp<label>$columnNames[$count]</label> <br>&nbsp&nbsp<input type='text' name=".$columnName
                 . " value=" . $row[$columnNames[$count]] . "><br><br>";
         }
     }
-    echo "<br><br>";
     for($count = 3; $count< count($columnNames); $count++){
         $isSelect = false;
         $columnName = $columnNames[$count];

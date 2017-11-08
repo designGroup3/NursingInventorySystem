2e9aa21775a5148cd2a82c474089ed3050f91615
mjlacy <mjlacy.dude@gmail.com>
2017-11-08 08:49:02 -0600

    Service agreements are now pdf's.

--- editServiceAgreement.php
+++ editServiceAgreement.php
@@ -1,22 +1,23 @@
 <?php
 include 'header.php';
-
 if(isset($_SESSION['id'])) {
     include 'dbh.php';
     $id = $_GET['edit'];
     echo "<head><Title>Edit Service Agreement</Title></head>";
-
+    $url ="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    if(strpos($url, 'error=wrongType') !== false){
+        echo "<br>&nbsp&nbspApproval forms must a .pdf file.<br>";
+    }
     $sql="SELECT * FROM serviceAgreements WHERE Id = $id";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
-
     echo "<form action ='includes/editServiceAgreement.inc.php' method ='POST' enctype='multipart/form-data'><br>
             <input type='hidden' name='id' value = $id>
             &nbsp&nbsp<label>Name:</label> <br>&nbsp&nbsp<input type='text' name='name' value='".$row['Name']."'><br><br>
             &nbsp&nbsp<label>Annual Cost:</label> <br>&nbsp&nbsp$<input type='number' name='cost' min='0' step='0.01' value='".$row['Annual Cost']."'><br><br>
             &nbsp&nbsp<label>Duration:</label> <br>&nbsp&nbsp<input type='text' name='duration' value='".$row['Duration']."'><br><br>
             &nbsp&nbsp<label>Expiration Date:</label> <br>&nbsp&nbsp<input type='date' name='date' value='".$row['Expiration Date']."'><br><br>
-            &nbsp&nbsp<label>Approval Form:</label> <br>&nbsp&nbsp<input type='file' name='approval'><br><br>
+            &nbsp&nbsp<label>Approval Form:</label> <br>&nbsp&nbsp<input type='file' name='file'><br><br>
             &nbsp&nbsp<button type='submit'>Submit</button>";
 }
 else{
--- includes/editServiceAgreement.inc.php
+++ includes/editServiceAgreement.inc.php
@@ -9,23 +9,41 @@ $cost = $_POST['cost'];
 $duration = $_POST['duration'];
 $date = $_POST['date'];
 
-$image = addslashes(file_get_contents($_FILES['approval']['tmp_name']));
-$image_size = getimagesize($_FILES['approval']['tmp_name']); //will return false if the file isn't an image
+//$image = addslashes(file_get_contents($_FILES['approval']['tmp_name']));
+//$image_size = getimagesize($_FILES['approval']['tmp_name']); //will return false if the file isn't an image
+//
+//if($image_size == FALSE && $image !== "") {
+//    header("Location: ../serviceAgreements.php?error=nonImage");
+//    exit();
+//}
 
-if($image_size == FALSE && $image !== "") {
-    header("Location: ../serviceAgreements.php?error=nonImage");
+$extension = end(explode(".", $_FILES["file"]["name"]));
+
+if ($_FILES['file']['error'] !== UPLOAD_ERR_OK) {
+    die("Upload failed with error " . $_FILES['file']['error']);
+}
+$finfo = finfo_open(FILEINFO_MIME_TYPE);
+$mime = finfo_file($finfo, $_FILES['file']['tmp_name']);
+
+if($mime == 'application/pdf') {
+    $sql = "UPDATE serviceAgreements SET Name = '$name', `Annual Cost` = '$cost', Duration = '$duration', `Expiration Date` = '$date', Approval = '$id' WHERE Id = '$id'";
+    $result = mysqli_query($conn, $sql);
+
+    move_uploaded_file($_FILES["file"]["tmp_name"], "../serviceAgreements/".$id .".pdf");
+
+    header("Location: ../serviceAgreements.php");
+}
+else{
+    header("Location: ../editServiceAgreement.php?edit=12&error=wrongType");
     exit();
 }
 
-if($image == ""){
-    $sql = "UPDATE serviceAgreements SET Name = '$name', `Annual Cost` = '$cost', Duration = '$duration', `Expiration Date` = '$date' WHERE Id = '$id'";
-}
-else{
-    $sql = "UPDATE serviceAgreements SET Name = '$name', `Annual Cost` = '$cost', Duration = '$duration', `Expiration Date` = '$date', Approval = '$image' WHERE Id = '$id'";
-}
-
-$result = mysqli_query($conn, $sql);
-
-header("Location: ../serviceAgreements.php");
-
+//if($image == ""){
+//    $sql = "UPDATE serviceAgreements SET Name = '$name', `Annual Cost` = '$cost', Duration = '$duration', `Expiration Date` = '$date' WHERE Id = '$id'";
+//}
+//else{
+//    $sql = "UPDATE serviceAgreements SET Name = '$name', `Annual Cost` = '$cost', Duration = '$duration', `Expiration Date` = '$date', Approval = '$image' WHERE Id = '$id'";
+//}
+//
+//$result = mysqli_query($conn, $sql);
 ?>

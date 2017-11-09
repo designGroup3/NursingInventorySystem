9d3f05c99ce4fcbdcc0780fb271557e5c3ba0020
HamadAlquait <Hamadbdr@gmail.com>
2017-11-09 16:11:36 -0600

    added required *
    added the required * on required field as per requested

--- signup.php
+++ signup.php
@@ -213,14 +213,14 @@
         <div class=\"form-group\"><label class=\"col-md-4 control-label\">First Name</label>  
         <div class=\"col-md-4 inputGroupContainer\"><div class=\"input-group\">
         <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>
-        <input name='first' placeholder='First Name' class=\"form-control\" type=\"text\"required></div></div></div>
+        <input name='first' placeholder='First Name' class=\"form-control\" type=\"text\"></div></div></div>
         
         <div class=\"form-group\"><label class=\"col-md-4 control-label\" >Last Name</label> 
         <div class=\"col-md-4 inputGroupContainer\"><div class=\"input-group\">
         <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>
         <input name='last' placeholder='Last Name' class='form-control'  type='text'></div></div></div>
         
-        <div class=\"form-group\"><label class=\"col-md-4 control-label\">E-Mail</label>  
+        <div class=\"form-group\"><label class=\"col-md-4 control-label\">E-Mail<p style=\"color:red;font-size:10px;\">*required</p></label>  
         <div class=\"col-md-4 inputGroupContainer\"><div class=\"input-group\">
         <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-envelope\"></i></span>
         <input name='email' placeholder='E-Mail Address' class='form-control'  type='email'required></div></div></div>
@@ -243,12 +243,12 @@
         }
 
         echo "</select></div></div></div>
-        <div class=\"form-group\"><label class=\"col-md-4 control-label\">Username</label>  
+        <div class=\"form-group\"><label class=\"col-md-4 control-label\">Username<p style=\"color:red;font-size:10px;\">*required</p></label>  
         <div class=\"col-md-4 inputGroupContainer\"><div class=\"input-group\">
         <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>
         <input type='text' class='form-control' placeholder='Username' name='uid'required></div></div></div>
         
-        <div class=\"form-group\"><label class=\"col-md-4 control-label\" for=\"psw\">Password</label>  
+        <div class=\"form-group\"><label class=\"col-md-4 control-label\" for=\"psw\">Password<p style=\"color:red;font-size:10px;\">*required</p></label>  
         <div class=\"col-md-4 inputGroupContainer\"><div class=\"input-group\">
         <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-lock\"></i></span>
         <input name='pwd' placeholder='Password' class='form-control' type='password' id='pwd'

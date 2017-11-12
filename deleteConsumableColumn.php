0b68b0734d843c2e6e0cc404eee8eeb5145d2f70
Michael <mjlacy.dude@gmail.com>
2017-11-11 20:24:34 -0600

    Implemented new front-end.

--- deleteConsumableColumn.php
+++ deleteConsumableColumn.php
@@ -20,31 +20,34 @@ if(isset($_SESSION['id'])) {
         array_push($columnNames, $row['Field']);
     }
 
-    echo '<br>&nbsp&nbspWhich column do you want to delete?
-    
-    <form method="post">
-        <label>
-            <br>&nbsp&nbsp<select name="column" onchange="this.form.submit()">
-                <option selected value=""></option>';
+    echo '<div class="container"><form method="post" class="well form-horizontal" id="contact_form"><fieldset>
+        <h2 align="center">Which column would you like to delete?</h2><br><div class="form-group">
+        <label class="col-md-4 control-label"></label>
+        <div class="col-md-4 selectContainer"><div class="input-group">
+        <span class="input-group-addon"><i class="fa fa-columns"></i></span>
+        <select name="column" onchange="this.form.submit()" class="form-control selectpicker"><option selected value=""></option>';
 
     for($columnsCount = 0; $columnsCount < count($columnNames); $columnsCount++) {
         if($columnsCount > 6){
             echo '<option value = "'.$columnNames[$columnsCount].'">'.$columnNames[$columnsCount].'</option>';
         }
     }
-    echo '</select></label></form>';
+    echo '</select></form></div></div></div>';
 
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $column = $_POST['column'];
 
-        echo "<br><p>&nbsp&nbspAre you sure you want to delete column ".$column."? All data it contains will be gone forever.</p><br>
-        <form action ='includes/deleteConsumableColumn.inc.php' method ='POST'>
-                <input type='hidden' name='column' value = \"$column\">
-                &nbsp&nbsp<button type='submit'>Delete</button>
-            </form><br>
-            <form action='consumables.php'>
-                &nbsp&nbsp<input type='submit' value='Cancel' />
-             </form>";
+        echo "<form action ='includes/deleteConsumableColumn.inc.php' method ='POST'>
+                
+            <fieldset><h3 align=\"center\">Are you sure you want to delete the $column column?</h3>
+            <p align=\"center\" style=\"color:red;\">*All data it contains will be gone forever.</p><br/>
+            <div class=\"form-group\" style='text-align: center'><label class=\"col-md-4 col-xs-4 col-sm-4 col-xl-4 control-label\"></label>
+            <div class='col-md-4 col-xs-4 col-lg-4 col-xl-4'><input type='hidden' name='column' value = \"$column\">
+            <input type=\"submit\" class=\"btn btn-danger\" value='Yes'>
+            <input onclick=\"window.location.href = 'consumables.php';\" class=\"btn btn-warning\" style='width:45px;' value='No'>
+
+            
+            </div></div><br><br></fieldset></form>";
     }
 }
 else{

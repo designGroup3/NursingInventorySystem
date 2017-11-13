342d12269c195f1f95c692af9f55a8417eb57821
mjlacy <mjlacy.dude@gmail.com>
2017-11-13 09:19:20 -0600

    Added new front-end.

--- addClient.php
+++ addClient.php
@@ -1,5 +1,6 @@
 <?php
 include 'header.php';
+include 'inputJS.php';
 
 if(isset($_SESSION['id'])) {
     include 'dbh.php';
@@ -7,13 +8,41 @@ if(isset($_SESSION['id'])) {
     echo "<head><Title>Add Client</Title></head><div class=\"parent\"><button class=\"help\" onclick=\"window.location.href='http://flowtime.be/wp-content/uploads/2016/01/Naamloosdocument.pdf'\">
         <i class='fa fa-question'></i></button></div>";
 
-    echo "<br><form action='includes/addClient.inc.php' method='POST'>
-        &nbsp&nbsp<label>Last Name<br></label>&nbsp&nbsp<input type='text' name='last'><br><br>
-        &nbsp&nbsp<label>First Name<br></label>&nbsp&nbsp<input type='text' name='first'><br><br>
-        &nbsp&nbsp<label>Ext.<br></label>&nbsp&nbsp<input type='number' min='0' name='ext'><br><br>
-        &nbsp&nbsp<label>Email<br></label>&nbsp&nbsp<input type='email' name='email'><br><br>
-        &nbsp&nbsp<label>Office<br></label>&nbsp&nbsp<input type='text' name='office'><br><br><br>
-        &nbsp&nbsp<button type='submit'>Add Client</button></form>";
+    echo "<div class=\"container\"><form action='includes/addClient.inc.php' class=\"well form-horizontal\" 
+        method='POST' id=\"contact_form\"><fieldset><h2 align=\"center\">Add Client</h2><br/>
+        
+        <div class=\"form-group\"><label class=\"col-md-4 control-label\">First Name:
+        <a style=\"color:red;\" title=\"This field must be filled\">*</a></label> 
+        <div class=\"col-md-4 inputGroupContainer\"><div class=\"input-group\">
+        <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>
+        <input type='text' required name='first' placeholder=\"First Name\" class=\"form-control\"></div></div></div>
+        
+        <div class=\"form-group\"><label class=\"col-md-4 control-label\" >Last Name:
+        <a style=\"color:red;\" title=\"This field must be filled\">*</a></label>
+        <div class=\"col-md-4 inputGroupContainer\"><div class=\"input-group\">
+        <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>
+        <input type='text' required name='last' placeholder=\"Last Name\" class=\"form-control\"></div></div></div>
+        
+        <div class=\"form-group\"><label class=\"col-md-4 control-label\" >Ext.
+        <a style=\"color:red;\" title=\"This field must be filled\">*</a></label> 
+        <div class=\"col-md-4 inputGroupContainer\"><div class=\"input-group\">
+        <span class=\"input-group-addon\"><i class=\"fa fa-phone-square\"></i></span>
+        <input type='number' required min='0' name='ext' placeholder=\"Extension\" class=\"form-control\"></div></div></div>
+        
+        <div class=\"form-group\"><label class=\"col-md-4 control-label\">E-Mail:
+        <a style=\"color:red;\" title=\"This field must be filled\">*</a></label>  
+        <div class=\"col-md-4 inputGroupContainer\"><div class=\"input-group\">
+        <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-envelope\"></i></span>
+        <input type='email' required name='email' placeholder=\"E-Mail Address\" class=\"form-control\"></div></div></div>
+        
+        <div class=\"form-group\"><label class=\"col-md-4 control-label\">Office:
+        <a style=\"color:red;\" title=\"This field must be filled\">*</a></label>  
+        <div class=\"col-md-4 inputGroupContainer\"><div class=\"input-group\">
+        <span class=\"input-group-addon\"><i class=\"fa fa-building\"></i></span>
+        <input type='text' required name='office' placeholder=\"Office\" class=\"form-control\"></div></div></div>
+        
+        <div class=\"form-group\"><label class=\"col-md-4 control-label\"></label><div class=\"col-md-4\">
+       <button type='submit' class='btn btn-warning btn-block'>Add Client</button></div></div></fieldset></form></div>";
 }
 else{
     header("Location: ./login.php");
